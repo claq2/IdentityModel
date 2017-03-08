77517921018e64649327d8c360fd0f071cc4bcae
James McLachlan <claquer@gmail.com>
2017-03-08 08:00:40 -0500

    Jwt extensions

---
+++ source/IdentityModel.Portable/Jwt/JwkNetExtensions.cs
@@ -0,0 +1,30 @@
+﻿using PCLCrypto;
+
+namespace IdentityModel.Jwt
+{
+    public static class JwkNetExtensions
+    {
+        public static JsonWebKey ToJsonWebKey(this ICryptographicKey key,
+            string alg = "RS256", string kid = null)
+        {
+            var parameters = key.ExportParameters(false);
+
+            var n = Base64Url.Encode(parameters.Modulus);
+            var e = Base64Url.Encode(parameters.Exponent);
+            return new JsonWebKey()
+            {
+                N = n,
+                E = e,
+                Kid = kid ?? CryptoRandom.CreateUniqueId(),
+                Kty = "RSA",
+                Alg = alg,
+            };
+        }
+
+        public static ICryptographicKey CreateKey(int keySize = 2048)
+        {
+            IAsymmetricKeyAlgorithmProvider provider = WinRTCrypto.AsymmetricKeyAlgorithmProvider.OpenAlgorithm(AsymmetricAlgorithm.RsaPkcs1);
+            return provider.CreateKeyPair(keySize);
+        }
+    }
+}
