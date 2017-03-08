45a331befbdc1e963a2bd250b35e44731f8382c2
James McLachlan <claquer@gmail.com>
2017-03-07 22:57:13 -0500

    CryptoRandom and the start of Phone 8.1 tests

---
+++ source/IdentityModel.Phone81.Tests/Class1.cs
@@ -0,0 +1,28 @@
+﻿using IdentityModel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityModel.Phone81.Tests
+{
+    [TestFixture]
+    public class CryptoRandomTests
+    {
+        [Test]
+        public void CreateUniqueIdDefaultLengthIs32()
+        {
+            var x = CryptoRandom.CreateUniqueId();
+            Assert.That(x.Length, Is.EqualTo(32));
+        }
+
+        [Test]
+        public void CanSpecifyLengthForCreateUniqueId()
+        {
+            var x = CryptoRandom.CreateUniqueId(1);
+            Assert.That(x.Length, Is.EqualTo(2));
+        }
+    }
+}
